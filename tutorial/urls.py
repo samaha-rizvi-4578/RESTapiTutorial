5a42066877558b2455456411c2001d691af0bbb3
rizvi <k214578@nu.edu.pk>
2024-04-17 15:07:27 +0500

    path hi theek kar rhi pata nhi kia masla hai

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from tutorial.quickstart import views
+from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
