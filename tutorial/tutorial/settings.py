edf3f2033e5e86bf337cc0541241941edea38ff0
rizvi <k214578@nu.edu.pk>
2024-04-17 15:42:34 +0500

    API tested

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -102,6 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': 
+        'rest_framework.permissions.IsAuthenticated',
+    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
