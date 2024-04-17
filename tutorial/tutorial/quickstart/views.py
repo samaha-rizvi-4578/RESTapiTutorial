77b1432dd300e2cef6fe26d29d7c09d06c56c7b7
rizvi <k214578@nu.edu.pk>
2024-04-17 13:58:21 +0500

    adding API endpoitn sin views.py

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
+
+# so basically these classes are API endpoints that allows user or group to be edited or viewed
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
